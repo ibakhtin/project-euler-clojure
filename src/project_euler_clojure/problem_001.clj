81f8c7df99afac2e127effdb74671b54fd5ac3c5
Igor Bakhtin <ibakhtin@icloud.com>
2023-01-10 16:58:46 +0300

    feat: solve problem 001

---
+++ src/project_euler_clojure/problem_001.clj
@@ -0,0 +1,19 @@
+(ns project-euler-clojure.problem-001)
+
+;; If we list all the natural numbers below 10 that are multiples
+;; of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defn divisible-by-3-or-5?
+  [number]
+  (or (zero? (mod number 3))
+      (zero? (mod number 5))))
+
+(def solution-001
+  (->>
+   (range 1000)
+   (filter divisible-by-3-or-5?)
+   (reduce +)))
+
+solution-001
